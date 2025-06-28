c822af91c0d180d395227fb677ec2d5c5e72016c
Ricardo-Puc <alex.puc@revenatium.com>
2025-06-27 23:31:26 -0600

    Se configura secrete_key por default

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-tx7z@4$%1k!n0r8s*2bqv#l9m&z7w+3f^u@o%ygxjhd4t!p",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG") == "True"
