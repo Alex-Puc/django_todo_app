a2264d413e34194c979151729b22c07507a7b0cb
Alex-Puc <arpkumul@gmail.com>
2025-06-28 23:55:45 -0600

    fix:cambio de nombre del proyecto

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TodoAppConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todo_app'
+    name = 'api'
