069f43325963f922c8a0c5a21d377f7b7424929f
Alex-Puc <arpkumul@gmail.com>
2025-07-02 12:28:42 -0600

    feat: creando super usuario para el admin

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py create_superuser 
 python -m gunicorn --bind 0.0.0.0:8000 --workers 3 xcaret.wsgi:application
